1f499a290d2ff8d75d9301e75870236a5d1721c0
Takács Bálint <tbalint99@gmail.com>
2022-07-19 00:33:59 +0200

    deploy on Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: false,
-  images: { domains: ['res.cloudinary.com'] }
+  images: { domains: ['res.cloudinary.com'] },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 // webpack(config) {
